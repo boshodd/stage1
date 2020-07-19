adb9e9defe5e7e52055ea7237414d13d4b069426
boshodd <66684265+boshodd@users.noreply.github.com>
2020-07-19 15:41:39 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+
+function myfun() {
+    var a = document.getElementById("ans").value.toUpperCase();
+    if (a == 'CORONA VIRUS')
+        window.location.href = 'https://glendon7.github.io/AIC/'
+    //                  alert("YOur ans is recorded");
+
+}
