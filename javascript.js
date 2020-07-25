5944d141df155c769b222b9071bf546cae9494e8
boshodd <66684265+boshodd@users.noreply.github.com>
2020-07-25 13:16:29 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,8 +2,8 @@
 function myfun() {
     var a = document.getElementById("ans").value.toUpperCase();
     if (a == 'CORONA VIRUS') 
-        window.location.href = 'https://glendon7.github.io/AIC/'
+    {window.location.href = 'https://glendon7.github.io/AIC/'}
     else if(a == 'CORONAVIRUS')
-            window.location.href = 'https://glendon7.github.io/AIC/'
-    else alert('Wrong answer,try again ');
+    { window.location.href = 'https://glendon7.github.io/AIC/'}
+    else {alert('Wrong answer,try again ');}
 }
