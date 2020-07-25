dc363bf1983eec649d21e60c27aa812a4104081f
boshodd <66684265+boshodd@users.noreply.github.com>
2020-07-25 13:09:00 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 
 function myfun() {
     var a = document.getElementById("ans").value.toUpperCase();
-    if (a == 'CORONA VIRUS')
+    if (a == 'CORONA VIRUS'|| a == 'CORONAVIRUS')
         window.location.href = 'https://glendon7.github.io/AIC/'
     //                  alert("YOur ans is recorded");
 
